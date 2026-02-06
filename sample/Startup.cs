6bc3d4438972a0c71552d8dfd419139f898e2532
MagicAndre1981 <MagicAndre1981@users.noreply.github.com>
2026-02-06 08:52:48 +0100

    - added net8.0;net9.0;net10.0 as TargetFramework - updated "Microsoft.AspNetCore.Mvc" to version 2.3.9 - added System.Text.RegularExpressions in Version 4.3.1 to netstandard2.0, as transitive version 4.3.0 has at least one vulnerability with high severity
    - Sample:
      - added net8.0;net9.0;net10.0 as TargetFramework
      - replaced WebMarkupMin.AspNetCore2 with WebMarkupMin.AspNetCoreLatest
      - replaced services.AddMvc(); with services.AddMvc(options => options.EnableEndpointRouting = false);
      - removed "using Microsoft.AspNetCore.Http.Internal;" call as namespace is not found in modern .net versions
      - removed "app.UseBrowserLink();" as only working until ASP.NET Core 2.2

--- sample/Startup.cs
+++ sample/Startup.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using WebEssentials.AspNetCore.OutputCaching;
-using WebMarkupMin.AspNetCore2;
+using WebMarkupMin.AspNetCoreLatest;
 
 namespace Sample
 {
@@ -13,7 +12,7 @@ namespace Sample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddOutputCaching(options =>
             {
                 options.Profiles["default"] = new OutputCacheProfile
@@ -37,7 +36,6 @@ namespace Sample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseBrowserLink();
             app.UseDeveloperExceptionPage();
 
             app.UseOutputCaching();
